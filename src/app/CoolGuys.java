ae3b95ac5123798448569d7894b885eb2c1bcc41
Jackh <eaajhsoer@students.eaaa.dk>
2022-02-22 11:16:02 +0100

    Innit Commit

--- src/app/CoolGuys.java
+++ src/app/CoolGuys.java
@@ -8,6 +8,12 @@ public class CoolGuys {
 
     //lol
 
+    public class RickRoll {
+        public static void main(String[] args) throws Exception {
+            java.awt.Desktop.getDesktop().browse(new java.net.URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+        }
+    }
+
 
 
 
