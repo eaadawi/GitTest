93ddbf43a307574d57c506a5fddcca58d7f7cc41
mathi <eaamatst@students.eaaa.dk>
2022-02-22 11:07:27 +0100

    Initial Commit

--- src/app/MathiasHelloWorld.java
+++ src/app/MathiasHelloWorld.java
@@ -2,9 +2,12 @@ package app;
 
 public class MathiasHelloWorld {
     public static void main(String[] args) {
+        isEven(2);
         System.out.println("Mathias siger hej til verden!");
     }
     public static boolean isEven(int number){
         return number % 2 == 0;
     }
+
+
 }
